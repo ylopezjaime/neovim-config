fde6fa75af3c8cb0aa0814b3bef2e20c39a2f172
ylopezjaime <ylopezjaime@gmail.com>
2024-06-06 22:28:14 -0400

    modified:   lua/plugins/none-ls.lua

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,14 +1,15 @@
 return {
-    "nvimtools/none-ls.nvim",
+  "nvimtools/none-ls.nvim",
 
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.diagnostics.selene,
-            },
-        })
-        vim.keymap.set("n", "<space>gf", vim.lsp.buf.format, {})
-    end,
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.selene,
+        null_ls.builtins.formatting.black,
+      },
+    })
+    vim.keymap.set("n", "<space>gf", vim.lsp.buf.format, {})
+  end,
 }
